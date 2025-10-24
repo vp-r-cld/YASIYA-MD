3ec9adcf76b0f8bafa0c07550ef6715e9f0a26da
vp-r-cld <senadiraupul16@gmail.com>
2025-10-24 21:52:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?SURybX'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?KDMyHe'
 };
